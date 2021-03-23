4904d66849bc24b703a864b2601ed80162f78f17
Árion Vidal Farias <focarion1337@protonmail.com>
2021-03-23 10:46:15 -0300

    get json request

--- myApp.js
+++ myApp.js
@@ -1,11 +1,10 @@
 var express = require('express');
 var app = express();
 
-app.get("/", function(req, res) {
-    app.use(express.static(__dirname + "/public"));
-    app.use("/public", express.static(__dirname + "/public"));
-    res.sendFile(__dirname + '/views/index.html');
-
+app.get("/json", (req, res) => {
+    res.json({
+      message: "Hello json"
+    });
   });
 
 
