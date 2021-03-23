cd2e9dce511e0ce447589b34ed233f0efc5e93d1
Árion Vidal Farias <focarion1337@protonmail.com>
2021-03-23 10:59:11 -0300

    get response

--- myApp.js
+++ myApp.js
@@ -1,9 +1,13 @@
 var express = require('express');
 var app = express();
-
+if (process.env.MESSAGE_STYLE === "uppercase") {
+    response = "Hello Json".toUpperCase();
+  } else {
+    response = "Hello Json";
+  }
 app.get("/json", (req, res) => {
     res.json({
-      message: "Hello json"
+      message: this.response
     });
   });
 
