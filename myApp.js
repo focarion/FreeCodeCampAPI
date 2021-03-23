7fec1d12d75aff338d0034c6984a46c7e9b4bb47
Árion Vidal Farias <focarion1337@protonmail.com>
2021-03-23 11:19:17 -0300

    update dotenv

--- myApp.js
+++ myApp.js
@@ -1,15 +1,21 @@
 var express = require('express');
 var app = express();
+require('dotenv').config()
+
+var response
+
 if (process.env.MESSAGE_STYLE === "uppercase") {
     response = "Hello Json".toUpperCase();
   } else {
     response = "Hello Json";
   }
+
 app.get("/json", (req, res) => {
     res.json({
-      message: this.response
+      message: response
     });
   });
+
 
 
 
