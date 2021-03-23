5efbb7b30a4ede776628a734809f761c4ccc337a
Árion Vidal Farias <focarion1337@protonmail.com>
2021-03-23 09:22:55 -0300

    update serve all public

--- myApp.js
+++ myApp.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get("/", function(req, res) {
-    res.sendFile(/views/index.html);
+    app.use("/public", express.static(__dirname + "/public"));
   });
 
 
