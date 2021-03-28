009c7b513f1aae3b9698cff178154bd9f6a0c8ff
Árion Vidal Farias <focarion1337@protonmail.com>
2021-03-28 13:14:01 -0300

    middleware function

--- myApp.js
+++ myApp.js
@@ -2,6 +2,11 @@ var express = require('express');
 var app = express();
 require('dotenv').config()
 
+app.use(function middleware(req, res, next) {
+   var string = req.method + " " + req.path + " - " + req.ip;
+   next();
+ });
+
 var absolutePath = __dirname + "/views/index.html"
    app.get("/", (req, res) => {res.sendFile(absolutePath)})
 
