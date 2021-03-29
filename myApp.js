515d5affe389c567faa04c20131b152c694bf346
Árion Vidal Farias <focarion1337@protonmail.com>
2021-03-29 05:39:36 -0300

    update app

--- myApp.js
+++ myApp.js
@@ -1,8 +1,7 @@
-const { Mongoose } = require('mongoose');
-
+const mongoose = require('mongoose');
+mongoose.connect(process.env.MONGO_URI);
 require('dotenv').config();
 
-Mongoose.connect(process.env.MONGO_URI,{ useNewUrlParser: true, useUnifiedTopology: true });
 let Person;
 
 const createAndSavePerson = (done) => {
