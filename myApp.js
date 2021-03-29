34cc8d64187f1728745ad8b0a093e6af0baa081c
Árion Vidal Farias <focarion1337@protonmail.com>
2021-03-29 09:06:56 -0300

    update mongodb app

--- myApp.js
+++ myApp.js
@@ -1,6 +1,18 @@
 const mongoose = require('mongoose');
-mongoose.connect(process.env.MONGO_URI);
 require('dotenv').config();
+mongoose
+    .connect(process.env.MONGO_URI, {
+        useNewUrlParser: true,
+        useCreateIndex: true,
+        useUnifiedTopology: true
+    })
+    .then(() => {
+        console.log('Connected to Mongo!');
+    })
+    .catch((err) => {
+        console.error('Error connecting to Mongo', err);
+    });
+
 
 let Person;
 
