7d3eba943716365fa0eefca90e1e8a9e1948bb95
Árion Vidal Farias <focarion1337@protonmail.com>
2021-03-31 11:43:52 -0300

    add query

--- myApp.js
+++ myApp.js
@@ -107,8 +107,14 @@ const removeManyPeople = (done) => {
 
 const queryChain = (done) => {
   const foodToSearch = "burrito";
-
-  done(null /*, data*/);
+  Person.find({ favoriteFoods: foodToSearch })
+  .sort({ name: -1 })
+  .limit(2)
+  .select({ age: 0 })
+  .exec(function(error, people) {
+    done(err, data);
+  });
+  
 };
 
 /** **Well Done !!**
